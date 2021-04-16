420eff44d0a220d652c6785b1c53994d9aa492b0
jadavean <davidanderson425@comcast.net>
2021-04-15 22:58:39 -0600

    Update Hello_Julia.jl

--- Hello_Julia.jl
+++ Hello_Julia.jl
@@ -7,3 +7,15 @@ end
 z = change_value!(x)
 print("change_value!(x) = " + z)
 print("x = " + x)
+
+x = [1,2,3]
+
+function change_array!(A)
+  A[1] = 5
+end
+
+y = change_array!(x)
+
+print("x = " + x)
+print("change_array!(x) = " + y)
+print("Now, x = " + x)
